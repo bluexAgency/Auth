999ac841f0e97b48d7786f04423e7fd30a63c349
samuelg <garavito350@gmail.com>
2025-03-19 15:28:21 -0400

    valicacion usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,12 @@
 import {Router} from "express";
-
 import userController from "../controllers/userController.js";
-
+import validateCreate from "../validators/users.js";
 
 const userRouter = Router();
 
 userRouter.get('/',userController.getAllUser);
 userRouter.get('/:id',userController.getUserById);
-userRouter.post('/',userController.createUser);
+userRouter.post('/',validateCreate,userController.createUser);
 userRouter.put('/:id',userController.updateUser);
 userRouter.delete('/:id',userController.deleteuser);
 
